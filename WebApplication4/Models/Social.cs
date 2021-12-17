e65276c24dfbb0c6c7fafc0bc06afb9088815bbf
Haji <tu201806253@aztu.local>
2021-12-17 11:03:48 +0400

    first

---
+++ WebApplication4/Models/Social.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication4.Models
+{
+    public class Social
+    {
+        [Key]
+        public int id { get; set; }
+        [MaxLength(30)]
+        public string name { get; set; }
+        [MaxLength(50)]
+        public string icon { get; set; }
+        [MaxLength(250)]
+        public string link { get; set; }
+    }
+}
---
+++ WebApplication4/Models/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication4.Models
+{
+    public class User
+    {
+        [Key]
+        public int id { get; set; }
+        [MaxLength(20)]
+        public string name { get; set; }
+        [MaxLength(20)]
+        public string surname { get; set; }
+        [MaxLength(50)]
+        public string email { set; get; }
+        [MaxLength(15)]
+        public string phone { get; set; }
+        [MaxLength(30)]
+        public string username { get; set; }
+        [MaxLength(250)]
+        public string password { get; set; }
+
+        public string publicDate { get; set; }
+
+    }
+}
